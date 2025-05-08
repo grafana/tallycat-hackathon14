7240ca368e1a6bce9a8e60c656f8ac5be688b7c1
Nicolas Takashi <nicolas.tcs@hotmail.com>
2025-05-08 22:14:15 +0100

    [CHORE] adding base ui structure shadcn
    Signed-off-by: Nicolas Takashi <nicolas.tcs@hotmail.com>

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+import { ThemeProvider } from './components/theme-provider'
 
 import './styles.css'
 import reportWebVitals from './reportWebVitals.ts'
@@ -31,7 +32,9 @@ if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </StrictMode>,
   )
 }
